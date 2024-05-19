2aa5f442201eb667bbefa11cc55907207fd1c469
josecespedess <joseadriancd.dam@gmail.com>
2024-05-19 22:58:20 +0200

    Diseño 1/1(inicioSesion,empleados50%)

--- src/main/java/ClasesObjetos/BDautoluxe.java
+++ src/main/java/ClasesObjetos/BDautoluxe.java
@@ -95,4 +95,153 @@ public class BDautoluxe
         }
         return listaVehiculos;
     }
+    //Método mostrar Empleados segun la opcion
+    public static List<Empleados> listadoEmpleadosBD(String opcion, String busqueda)
+    {
+        List<Empleados> listaVehiculos = FXCollections.observableArrayList();
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        if(opcion=="Nombre")
+        {
+            try
+            {
+                st=connection.prepareStatement("SELECT * FROM empleados WHERE nombre='"+busqueda+"'");
+
+                rs=st.executeQuery();
+                while (rs.next()) {
+                    String DNI = rs.getString("DNI");
+                    String nombre = rs.getString("nombre");
+                    String apellidos = rs.getString("apellidos");
+                    String rol = rs.getString("rol");
+                    String telefono = rs.getString("telefono");
+                    String correo = rs.getString("correo");
+                    String contrasena = rs.getString("contraseña");
+                    Empleados empleado = new Empleados(DNI, nombre, apellidos,telefono,rol,correo,contrasena);
+                    listaVehiculos.add(empleado);
+                }
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        else if(opcion=="Apellidos")
+        {
+            try
+            {
+                st=connection.prepareStatement("SELECT * FROM empleados WHERE apellidos='"+busqueda+"'");
+
+                rs=st.executeQuery();
+                while (rs.next()) {
+                    String DNI = rs.getString("DNI");
+                    String nombre = rs.getString("nombre");
+                    String apellidos = rs.getString("apellidos");
+                    String rol = rs.getString("rol");
+                    String telefono = rs.getString("telefono");
+                    String correo = rs.getString("correo");
+                    String contrasena = rs.getString("contraseña");
+                    Empleados empleado = new Empleados(DNI, nombre, apellidos,telefono,rol,correo,contrasena);
+                    listaVehiculos.add(empleado);
+                }
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        else if(opcion=="Rol")
+        {
+            try
+            {
+                st=connection.prepareStatement("SELECT * FROM empleados WHERE rol='"+busqueda+"'");
+
+                rs=st.executeQuery();
+                while (rs.next()) {
+                    String DNI = rs.getString("DNI");
+                    String nombre = rs.getString("nombre");
+                    String apellidos = rs.getString("apellidos");
+                    String rol = rs.getString("rol");
+                    String telefono = rs.getString("telefono");
+                    String correo = rs.getString("correo");
+                    String contrasena = rs.getString("contraseña");
+                    Empleados empleado = new Empleados(DNI, nombre, apellidos,telefono,rol,correo,contrasena);
+                    listaVehiculos.add(empleado);
+                }
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        else if(opcion=="DNI")
+        {
+            try
+            {
+                st=connection.prepareStatement("SELECT * FROM empleados WHERE DNI='"+busqueda+"'");
+
+                rs=st.executeQuery();
+                while (rs.next()) {
+                    String DNI = rs.getString("DNI");
+                    String nombre = rs.getString("nombre");
+                    String apellidos = rs.getString("apellidos");
+                    String rol = rs.getString("rol");
+                    String telefono = rs.getString("telefono");
+                    String correo = rs.getString("correo");
+                    String contrasena = rs.getString("contraseña");
+                    Empleados empleado = new Empleados(DNI, nombre, apellidos,telefono,rol,correo,contrasena);
+                    listaVehiculos.add(empleado);
+                }
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return listaVehiculos;
+    }
+    //Método para obtener un Empleado[DNI]
+    public static Empleados obtenerEmpleadoDNI(String busqueda)
+    {
+        Empleados e = null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try
+        {
+            st=connection.prepareStatement("SELECT * FROM empleados WHERE DNI='"+busqueda+"'");
+
+            rs=st.executeQuery();
+            while (rs.next()) {
+                String DNI = rs.getString("DNI");
+                String nombre = rs.getString("nombre");
+                String apellidos = rs.getString("apellidos");
+                String rol = rs.getString("rol");
+                String telefono = rs.getString("telefono");
+                String correo = rs.getString("correo");
+                String contrasena = rs.getString("contraseña");
+                e = new Empleados(DNI, nombre, apellidos,telefono,rol,correo,contrasena);
+                
+            }
+        }
+        catch (SQLException i)
+        {
+            i.printStackTrace();
+        }
+        return e;
+    }
+    //Método para borrar un Empleado[DNI]
+    public static void borrarEmpleadoBD(String busqueda)
+    {
+        PreparedStatement st = null;
+        try
+        {
+            String query = "DELETE FROM empleados WHERE DNI=?";
+            st = connection.prepareStatement(query);
+            st.setString(1, busqueda);
+            st.executeUpdate();
+        }
+        catch (SQLException i)
+        {
+            i.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/example/autoluxe/ControladorEmpleadosYRoles.java
+++ src/main/java/com/example/autoluxe/ControladorEmpleadosYRoles.java
@@ -2,6 +2,7 @@ package com.example.autoluxe;
 
 import ClasesObjetos.BDautoluxe;
 import ClasesObjetos.Empleados;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,6 +15,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -52,8 +54,37 @@ public class ControladorEmpleadosYRoles  {
     private Button btnBuscar;
     @FXML
     private ChoiceBox<String> cbBusquedaEmpleadosA;
-    private String[] opcionesBuscarEmpleado = {"DNI", "nombre","apellidos", "rol"};
-    //Tabla Buscar Empleados
+    private String[] opcionesBuscarEmpleado = {"General","DNI", "Nombre","Apellidos", "Rol"};
+    //Panel de Editar Empleado
+    @FXML
+    private TextField edNombreA1;
+    @FXML
+    private TextField edDNIA1;
+    @FXML
+    private TextField edApellidosA1;
+    @FXML
+    private TextField edCorreoA1;
+    @FXML
+    private TextField edTlfnoA1;
+    @FXML
+    private PasswordField edContrasenaA1;
+    @FXML
+    private TextField edBuscar1;
+    @FXML
+    private Button btnBuscar1;
+    @FXML
+    private TextField edBuscar2;
+    @FXML
+    private Button btnBuscar2;
+    @FXML
+    private ChoiceBox<String> choiceBoxA1;
+    @FXML
+    private ChoiceBox<String> cbBusquedaEmpleadosA1;
+    @FXML
+    private Button acBorrar;
+    @FXML
+    private Button acEditar;
+    //Tabla Buscar Empleados [Añadir Empleados]
     @FXML
     private TableView<Empleados> vistaEmpleadosA;
     @FXML
@@ -69,6 +100,36 @@ public class ControladorEmpleadosYRoles  {
     @FXML
     private TableColumn<Empleados, String> colTelefonoEmpleadoA;
     //Tabla Empleado Añadido
+    @FXML
+    private TableView<Empleados> vistaEmpleadoAnadido;
+    @FXML
+    private TableColumn<Empleados, String> colDNIEmpleadoA1;
+    @FXML
+    private TableColumn<Empleados, String> colNombreEmpleadoA1;
+    @FXML
+    private TableColumn<Empleados, String> colApellidosEmpleadoA1;
+    @FXML
+    private TableColumn<Empleados, String> colRolEmpleadoA1;
+    @FXML
+    private TableColumn<Empleados, String> colCorreoEmpleadoA1;
+    @FXML
+    private TableColumn<Empleados, String> colTelefonoEmpleadoA1;
+    //Tabla Buscar Empleados [Editar Empleados]
+    @FXML
+    private TableView<Empleados> vistaEmpleadosA2;
+    @FXML
+    private TableColumn<Empleados, String> colDNIEmpleadoA2;
+    @FXML
+    private TableColumn<Empleados, String> colNombreEmpleadoA2;
+    @FXML
+    private TableColumn<Empleados, String> colApellidosEmpleadoA2;
+    @FXML
+    private TableColumn<Empleados, String> colRolEmpleadoA2;
+    @FXML
+    private TableColumn<Empleados, String> colCorreoEmpleadoA2;
+    @FXML
+    private TableColumn<Empleados, String> colTelefonoEmpleadoA2;
+
 
     public void initialize() throws SQLException, ClassNotFoundException {
         //Aplicamos la lista al ChoiceBox
@@ -77,9 +138,16 @@ public class ControladorEmpleadosYRoles  {
         //Aplicamos la lista al ChoiceBox
         cbBusquedaEmpleadosA.getItems().addAll(opcionesBuscarEmpleado);
         cbBusquedaEmpleadosA.getSelectionModel().select(0);
+        //Aplicamos la lista al ChoiceBox
+        choiceBoxA1.getItems().addAll(roles);
+        choiceBoxA1.getSelectionModel().select(1);
+        //Aplicamos la lista al ChoiceBox
+        cbBusquedaEmpleadosA1.getItems().addAll(opcionesBuscarEmpleado);
+        cbBusquedaEmpleadosA1.getSelectionModel().select(0);
         //Aplicamos la lista de todos los empleados en la Tabla Empleados
         establecerEmpleados();
         iniciarColumnas();
+
     }
     @FXML
     private void cerrarVentana() {
@@ -163,17 +231,163 @@ public class ControladorEmpleadosYRoles  {
             }
             else
             {
-                Empleados empleado=new Empleados(edDNIA.getText(),edNombreA.getText(),edApellidosA.getText(),edTlfnoA.getText(),cbBusquedaEmpleadosA.getValue().toString(),edCorreoA.getText(),edContrasenaA.getText());
+                Empleados empleado=new Empleados(edDNIA.getText(),edNombreA.getText(),edApellidosA.getText(),edTlfnoA.getText(),choiceBoxA.getValue(),edCorreoA.getText(),edContrasenaA.getText());
                 BDautoluxe.altaEmpleadoBD(empleado);
+                establecerEmpleadoAnadido(empleado);
             }
         }
         bd.desconectar();
     }
     public void establecerEmpleados() throws SQLException, ClassNotFoundException {
+        vistaEmpleadosA.getItems().clear();
         bd.conectar();
         List<Empleados> listaEmpleados=BDautoluxe.listadoEmpleadosBD();
         vistaEmpleadosA.setItems((ObservableList<Empleados>) listaEmpleados);
+        vistaEmpleadosA2.setItems((ObservableList<Empleados>) listaEmpleados);
         bd.desconectar();
+    }
+    public void establecerEmpleadoAnadido(Empleados empleado)
+    {
+        vistaEmpleadoAnadido.getItems().clear();
+        ObservableList<Empleados> listaEmpleado = FXCollections.observableArrayList();
+        listaEmpleado.add(empleado);
+        vistaEmpleadoAnadido.setItems(listaEmpleado);
+    }
+    //Método para buscar en la tabla Empleados
+    @FXML
+    public void buscarDatosTablaEmpleados() throws SQLException, ClassNotFoundException {
+        vistaEmpleadosA.getItems().clear();
+        String opcion=cbBusquedaEmpleadosA.getValue();
+        switch(opcion)
+        {
+            case "General"-> establecerEmpleados();
+            default ->{
+                bd.conectar();
+                List<Empleados> listaEmpleados=BDautoluxe.listadoEmpleadosBD(opcion,edBuscar.getText());
+                vistaEmpleadosA.setItems((ObservableList<Empleados>)listaEmpleados);
+                bd.desconectar();
+            }
+        }
+    }
+    //Método para buscar en la tabla Empleados 2
+    @FXML
+    public void buscarDatosTablaEmpleados2() throws SQLException, ClassNotFoundException {
+        vistaEmpleadosA2.getItems().clear();
+        String opcion=cbBusquedaEmpleadosA1.getValue();
+        switch(opcion)
+        {
+            case "General"-> establecerEmpleados();
+            default ->{
+                bd.conectar();
+                List<Empleados> listaEmpleados=BDautoluxe.listadoEmpleadosBD(opcion,edBuscar2.getText());
+                vistaEmpleadosA2.setItems((ObservableList<Empleados>)listaEmpleados);
+                bd.desconectar();
+            }
+        }
+    }
+    //Método para buscar Empleado
+    @FXML
+    public void buscarEmpleado() throws  ClassNotFoundException {
+        try
+        {
+            bd.conectar();
+            Empleados empleado=BDautoluxe.obtenerEmpleadoDNI(edBuscar1.getText());
+            if(empleado==null)
+            {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Diálogo de Alerta");
+                alert.setHeaderText("DNI no existente");
+                alert.showAndWait();
+            }
+            else
+            {
+                edNombreA1.setText(empleado.getNombre());
+                edApellidosA1.setText(empleado.getApellidos());
+                edDNIA1.setText(empleado.getDNI());
+                edTlfnoA1.setText(empleado.getTelefono());
+                choiceBoxA1.setValue(empleado.getRol());
+                edCorreoA1.setText(empleado.getCorreo());
+                edContrasenaA1.setText(empleado.getContrasena());
+
+                edNombreA1.setDisable(false);
+                edApellidosA1.setDisable(false);
+                edDNIA1.setDisable(false);
+                edTlfnoA1.setDisable(false);
+                choiceBoxA1.setDisable(false);
+                edCorreoA1.setDisable(false);
+                edContrasenaA1.setDisable(false);
+                acBorrar.setDisable(false);
+                acEditar.setDisable(false);
+            }
+            bd.desconectar();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+    //Método para borrar Empleado
+    @FXML
+    public void borrarEmpleado() throws ClassNotFoundException {
+        try
+        {
+            bd.conectar();
+            BDautoluxe.borrarEmpleadoBD(edDNIA1.getText());
+            edNombreA1.clear();
+            edApellidosA1.clear();
+            edDNIA1.clear();
+            edTlfnoA1.clear();
+            choiceBoxA1.setValue("");
+            edCorreoA1.clear();
+            edContrasenaA1.clear();
+
+            edNombreA1.setDisable(true);
+            edApellidosA1.setDisable(true);
+            edDNIA1.setDisable(true);
+            edTlfnoA1.setDisable(true);
+            choiceBoxA1.setDisable(true);
+            edCorreoA1.setDisable(true);
+            edContrasenaA1.setDisable(true);
+            acBorrar.setDisable(true);
+            acEditar.setDisable(true);
+            bd.desconectar();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    //Método para actualizar las tablas
+    @FXML
+    public void actualizarTablas() throws SQLException, ClassNotFoundException {
+        establecerEmpleados();
+
+        vistaEmpleadoAnadido.getItems().clear();
+    }
+    //Método para inicializar las columnas y que se vean
+    private void iniciarColumnas()
+    {
+        colDNIEmpleadoA.setCellValueFactory(new PropertyValueFactory<Empleados,String>("DNI"));
+        colNombreEmpleadoA.setCellValueFactory(new PropertyValueFactory<Empleados,String>("nombre"));
+        colApellidosEmpleadoA.setCellValueFactory(new PropertyValueFactory<Empleados,String>("apellidos"));
+        colRolEmpleadoA.setCellValueFactory(new PropertyValueFactory<Empleados,String>("rol"));
+        colCorreoEmpleadoA.setCellValueFactory(new PropertyValueFactory<Empleados,String>("correo"));
+        colTelefonoEmpleadoA.setCellValueFactory(new PropertyValueFactory<Empleados,String>("telefono"));
+
+        colDNIEmpleadoA1.setCellValueFactory(new PropertyValueFactory<Empleados,String>("DNI"));
+        colNombreEmpleadoA1.setCellValueFactory(new PropertyValueFactory<Empleados,String>("nombre"));
+        colApellidosEmpleadoA1.setCellValueFactory(new PropertyValueFactory<Empleados,String>("apellidos"));
+        colRolEmpleadoA1.setCellValueFactory(new PropertyValueFactory<Empleados,String>("rol"));
+        colCorreoEmpleadoA1.setCellValueFactory(new PropertyValueFactory<Empleados,String>("correo"));
+        colTelefonoEmpleadoA1.setCellValueFactory(new PropertyValueFactory<Empleados,String>("telefono"));
+
+        colDNIEmpleadoA2.setCellValueFactory(new PropertyValueFactory<Empleados,String>("DNI"));
+        colNombreEmpleadoA2.setCellValueFactory(new PropertyValueFactory<Empleados,String>("nombre"));
+        colApellidosEmpleadoA2.setCellValueFactory(new PropertyValueFactory<Empleados,String>("apellidos"));
+        colRolEmpleadoA2.setCellValueFactory(new PropertyValueFactory<Empleados,String>("rol"));
+        colCorreoEmpleadoA2.setCellValueFactory(new PropertyValueFactory<Empleados,String>("correo"));
+        colTelefonoEmpleadoA2.setCellValueFactory(new PropertyValueFactory<Empleados,String>("telefono"));
     }
     /*
     MENU 8/8
@@ -287,14 +501,5 @@ public class ControladorEmpleadosYRoles  {
         {
             e.printStackTrace();
         }
-    }
-    private void iniciarColumnas()
-    {
-        colDNIEmpleadoA.setCellValueFactory(new PropertyValueFactory<Empleados,String>("DNI"));
-        colNombreEmpleadoA.setCellValueFactory(new PropertyValueFactory<Empleados,String>("nombre"));
-        colApellidosEmpleadoA.setCellValueFactory(new PropertyValueFactory<Empleados,String>("apellidos"));
-        colRolEmpleadoA.setCellValueFactory(new PropertyValueFactory<Empleados,String>("rol"));
-        colCorreoEmpleadoA.setCellValueFactory(new PropertyValueFactory<Empleados,String>("correo"));
-        colTelefonoEmpleadoA.setCellValueFactory(new PropertyValueFactory<Empleados,String>("telefono"));
     }
 }
